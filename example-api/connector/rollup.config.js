5e8d0a9a2f16b2fca3a994a991371f951f68f770
tmf <tom.forrer@gmail.com>
2017-07-28 10:30:31 +0200

    feat: set up rollup
    to include external npm packages in build

--- example-api/connector/rollup.config.js
+++ example-api/connector/rollup.config.js
@@ -1,12 +1,21 @@
 import babel from 'rollup-plugin-babel';
 
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
     entry: 'src/index.js',
     dest: 'dist/example-api-connector.js',
     format: 'cjs',
     plugins:[
+        resolve(),
+        commonjs({
+            include: 'node_modules/**'
+        }),
         babel({
-
-        })
+            runtimeHelpers: true,
+            externalHelpers: true,
+            exclude: 'node_modules/**'
+        }),
     ]
 };
