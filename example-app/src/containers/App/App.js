754258fcd07b725a60ed0f22569a7c039f5d1cd8
tmf <tom.forrer@gmail.com>
2017-07-28 10:48:05 +0200

    style: reformat

--- example-app/src/containers/App/App.js
+++ example-app/src/containers/App/App.js
@@ -6,21 +6,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="App-intro">
-            <AddTodo />
-            <VisibleTodoList />
-            <Footer />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <div className="App-intro">
+                    <AddTodo/>
+                    <VisibleTodoList/>
+                    <Footer/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
